92f7eab487c6d874cc904a0b6eb91e524efa9fe9
Binary-Human <mll.sara.bakri@gmail.com>
2024-10-12 21:26:01 +0200

    added script to access the evolution of inertia and runtime across multiple k values

---
+++ extrait-code/archive-code/2.1-Best--k-means-search.py
@@ -0,0 +1,51 @@
+import os
+os.environ["OMP_NUM_THREADS"] = '4'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import pandas as pd
+
+import csv
+
+from scipy.io import arff
+from sklearn import cluster
+from sklearn import metrics
+
+from Starting_with_k_means import run_clustering
+
+
+
+# Define the range of clusters k to test
+k_values = range(2, 11)  # You can adjust the range depending on your needs
+_path = './artificial/'
+_name="xclara.arff"
+
+for k in k_values:
+    run_clustering(_path, _name, k, 0)
+
+data = pd.read_csv("results.csv")
+
+k_values = data['k']              # Number of clusters
+runtime = data['runtime_ms']    # Runtime in milliseconds
+inertia = data['inertia']         # Inertia
+
+# Step 3: Create the first plot (Inertia vs K)
+plt.figure(figsize=(10, 6))
+plt.plot(k_values, inertia, label='Inertia', marker='o', color='red')
+plt.title('Inertia vs Number of Clusters (K)')
+plt.xlabel('Number of Clusters (K)')
+plt.ylabel('Inertia')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+# Step 4: Create the second plot (Runtime vs K)
+plt.figure(figsize=(10, 6))
+plt.plot(k_values, runtime, label='Runtime (ms)', marker='x', color='blue')
+plt.title('Runtime vs Number of Clusters (K)')
+plt.xlabel('Number of Clusters (K)')
+plt.ylabel('Runtime (ms)')
+plt.grid(True)
+plt.legend()
+plt.show()
---
+++ extrait-code/archive-code/Starting_with_k_means.py
@@ -0,0 +1,89 @@
+"""
+Created on 2023/09/11
+
+@author: huguet
+"""
+import os
+os.environ["OMP_NUM_THREADS"] = '4'
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+import csv
+
+from scipy.io import arff
+from sklearn import cluster
+from sklearn import metrics
+
+##################################################################
+# Exemple :  k-Means Clustering
+
+_path = './artificial/'
+_name="xclara.arff"
+
+def run_clustering(path, name, k, showplot):
+    #path_out = './fig/'
+    databrut = arff.loadarff(open(path+str(name), 'r'))
+    datanp = np.array([[x[0],x[1]] for x in databrut[0]])
+
+    # PLOT datanp (en 2D) - / scatter plot
+    # Extraire chaque valeur de features pour en faire une liste
+    # EX : 
+    # - pour t1=t[:,0] --> [1, 3, 5, 7]
+    # - pour t2=t[:,1] --> [2, 4, 6, 8]
+    print("---------------------------------------")
+    print("Affichage données initiales            "+ str(name))
+    f0 = datanp[:,0] # tous les élements de la première colonne
+    f1 = datanp[:,1] # tous les éléments de la deuxième colonne
+
+    if showplot :
+        #plt.figure(figsize=(6, 6))
+        plt.scatter(f0, f1, s=8)
+        plt.title("Donnees initiales : "+ str(name))
+        #plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-init.jpg",bbox_inches='tight', pad_inches=0.1)
+        plt.show()
+
+
+    with open('results.csv', 'a', newline='') as results_file:
+        results_writer = csv.writer(results_file)
+
+        # Write headers for the CSV files
+        # Leave out but need to write beforehand
+        # results_writer.writerow(['k', 'runtime_ms','inertia' ])
+            
+        # Run clustering method for a given number of clusters
+        print("------------------------------------------------------")
+        print("Appel KMeans pour une valeur de k fixée")
+        tps1 = time.time()
+        model = cluster.KMeans(n_clusters=k, init='k-means++', n_init=1)
+        model.fit(datanp)
+        tps2 = time.time()
+        labels = model.labels_
+        # informations sur le clustering obtenu
+        iteration = model.n_iter_
+        inertie = model.inertia_
+        centroids = model.cluster_centers_
+        runtime = round((tps2 - tps1)*1000,2)
+
+        # Entering data
+        results_writer.writerow([k, runtime, inertie])
+        print( runtime, ", ", inertie)  
+
+        if showplot :
+            #plt.figure(figsize=(6, 6))
+            plt.scatter(f0, f1, c=labels, s=8)
+            plt.scatter(centroids[:, 0],centroids[:, 1], marker="x", s=50, linewidths=3, color="red")
+            plt.title("Données après clustering : "+ str(name) + " - Nb clusters ="+ str(k))
+            #plt.savefig(path_out+"Plot-kmeans-code1-"+str(name)+"-cluster.jpg",bbox_inches='tight', pad_inches=0.1)
+            plt.show()
+
+            print("nb clusters =",k,", nb iter =",iteration, ", inertie = ", inertie, ", runtime = ", runtime,"ms")
+            #print("labels", labels)
+
+            from sklearn.metrics.pairwise import euclidean_distances
+            dists = euclidean_distances(centroids)
+            print(dists)
+
+
+#run_clustering(_path, _name, 3, 1)
