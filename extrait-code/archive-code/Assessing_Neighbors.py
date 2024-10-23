23f4adf161216ed9e12e6bdccfa3365fd1d19e9c
Binary-Human <mll.sara.bakri@gmail.com>
2024-10-23 18:01:08 +0200

    changed langage of distancetoNeighbor outputs

--- extrait-code/archive-code/Assessing_Neighbors.py
+++ extrait-code/archive-code/Assessing_Neighbors.py
@@ -1,3 +1,4 @@
+                      
 import os
 os.environ["OMP_NUM_THREADS"] = '4'
 
@@ -15,7 +16,7 @@ from sklearn import cluster
 from sklearn import metrics
 from sklearn.neighbors import NearestNeighbors
  
-def distanceToNeighbors(path, name, v, showplot) :
+def distanceToNeighbors(path, name, v, showplot):
     """
     Calculates the distance to the v-th nearest neighbor for each data point
     and returns the 98th percentile of these distances. This can be used to 
@@ -32,41 +33,44 @@ def distanceToNeighbors(path, name, v, showplot) :
     """
     
     #path_out = './fig/'
-    databrut = arff.loadarff(open(path+str(name), 'r'))
-    datanp = np.array([[x[0],x[1]] for x in databrut[0]])
+    databrut = arff.loadarff(open(path + str(name), 'r'))
+    datanp = np.array([[x[0], x[1]] for x in databrut[0]])
 
-    # PLOT datanp (en 2D) - / scatter plot
-    # Extraire chaque valeur de features pour en faire une liste
-    # EX : 
-    # - pour t1=t[:,0] --> [1, 3, 5, 7]
-    # - pour t2=t[:,1] --> [2, 4, 6, 8]
+    # PLOT datanp (in 2D) - / scatter plot
+    # Extract each feature value to make a list
+    # EX:
+    # - for t1=t[:,0] --> [1, 3, 5, 7]
+    # - for t2=t[:,1] --> [2, 4, 6, 8]
     print("---------------------------------------")
-    print("Affichage données initiales            "+ str(name))
-    f0 = datanp[:,0] # tous les élements de la première colonne
-    f1 = datanp[:,1] # tous les éléments de la deuxième colonne
+    print("Displaying initial data               " + str(name))
+    f0 = datanp[:, 0]  # all elements of the first column
+    f1 = datanp[:, 1]  # all elements of the second column
 
-    # Distances aux k plus proches voisins
-    # Donnees dans X
+    # Distances to the k nearest neighbors
+    # Data in X
     v = 5
-    neigh = NearestNeighbors(n_neighbors = v)
+    neigh = NearestNeighbors(n_neighbors=v)
     neigh.fit(datanp)
-    distances , indices = neigh.kneighbors(datanp)
+    distances, indices = neigh.kneighbors(datanp)
 
     # distance between points
     #
-    # distance moyenne sur les k plus proches voisins
-    # en retirant le point " origine "
-    newDistances = np.asarray([np.average(distances[i][1:]) for i in range(0, distances.shape[0])] )
-    # trier par ordre croissant
-    distancetrie = np.sort(newDistances) 
+    # average distance to the k nearest neighbors
+    # excluding the "origin" point
+    newDistances = np.asarray([np.average(distances[i][1:]) for i in range(0, distances.shape[0])])
+    # sort in ascending order
+    distancetrie = np.sort(newDistances)
 
-    if showplot :
-        plt.title(f"Distances aux {str(v)} proches voisins pour tous les points" )
-        plt.xlabel('"id" du point dans le modèle')
+    if showplot:
+        plt.title(f"Distances to the {str(v)} nearest neighbors for all points")
+        plt.xlabel('"id" of the point in the model')
         plt.ylabel("distance")
         plt.plot(distancetrie)
         plt.show()
 
-    return np.percentile(distancetrie,98)
+    return np.percentile(distancetrie, 98)
 
 distanceToNeighbors('./extrait-code/artificial/', sys.argv[1], sys.argv[2], 1)
+
+
+                  
